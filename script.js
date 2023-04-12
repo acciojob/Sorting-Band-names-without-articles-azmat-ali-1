bd9db602e35c32944a5aecc34ee9c384591f1b33
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-13 00:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
 
+ let bandNames = ["The Beatles", "Aerosmith", "The Who", "Led Zeppelin", "Queen"];
+
+      // Remove articles from band names and sort them
+      let sortedBandNames = bandNames
+        .map((name) => name.replace(/^(a|an|the)\s+/i, ""))
+        .sort();
+
+      // Print sorted band names in an unordered list
+      let bandList = document.getElementById("band");
+      sortedBandNames.forEach((name) => {
+        let li = document.createElement("li");
+        li.innerText = name;
+        bandList.appendChild(li);
+      });
