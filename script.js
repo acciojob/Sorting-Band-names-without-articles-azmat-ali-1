57f84621c11cb167e29c341be3138917dd744ba6
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-05-28 15:29:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,9 @@
       // Remove articles from band names and sort them
       let sortedBandNames = bandNames
         .map((name) => name.replace(/^(a|an|the)\s+/i, ""))
-        .sort();
+        .sort((a,b){
+			a-b;
+		});
 
       // Print sorted band names in an unordered list
       let bandList = document.getElementById("band");
